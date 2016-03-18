27e2e6868cd428af7eafaffbb86ad76aea51f1cd
Dan Flettre <fletd01@yahoo.com>
2016-03-18 00:08:36 -0500

    leverage devDeps and add 50k guid test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var isGuid = require('./index');
+var aguid = require('aguid');
 
 describe('Test string object type', function () {
     it('This is not a string', function () {
@@ -18,5 +19,13 @@ describe('Test valid guid string', function () {
         assert.equal(true, isGuid('d3aa88e2-c754-41e0-8ba6-4198a34aa0a2'));
         assert.equal(true, isGuid('{d3aa88e2-c754-41e0-8ba6-4198a34aa0a2}'));
         assert.equal(true, isGuid('{DF8B6CD9-A928-4FA0-B106-DE183AC05DAA}'));
+    });
+});
+
+describe('Test 50,000 guid strings', function () {
+    it('validated 50,000 guids', function () {
+        for (var i = 0; i < 50000; i++) {
+            assert.equal(true, isGuid(aguid()));
+        }
     });
 });
